82fb5d3c63c8b75c75719975d45aaebc3769df9b
Alexis <alexis.benamar@viacesi.fr>
2017-03-20 16:27:19 +0100

    added animations + skills

--- js/script.js
+++ js/script.js
@@ -14,4 +14,46 @@ $(document).ready(function() {
         }, 1000, "easeInOutQuad");
     });
 
+    $("#skills").on( 'scroll', function(){
+       console.log('Event Fired');
+    });
+
+    var options = [
+        {
+            selector: '#headlines', offset: 200, callback: function() {
+                $('#headlines').css({
+                    "transition": "1s ease",
+                    "opacity": "1",
+                    "margin-left": "0"
+                });
+            }
+        },
+        {
+            selector: '.about-me', offset: 200, callback: function() {
+                $('.profile-pic').css({
+                    "transition": "1s ease",
+                    "opacity": "1"
+                });
+                $('#about-me-text').css({
+                    "transition": "1s ease",
+                    "opacity": "1",
+                    "margin-left": "0"
+                });
+            }
+        },
+        {
+            selector: '.skill-section', offset: 200, callback: function() {
+                $('.skill-section').css({
+                    "transition": "1s ease",
+                    "opacity": "1"
+                });
+                $('.skill-rating').css({
+                    "transition": "1.5s ease",
+                    "width": "100%"
+                });
+            }
+        },
+    ];
+    Materialize.scrollFire(options);
+
 });
